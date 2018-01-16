789265895d30daf04064977b767420c544c154be
Griffin P Longtin and Mikey Pinaud <mikey@>
2018-01-16 12:00:50 -0800

    add name and ingredient

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class='jumbotron container'>
-    <h1>Hello</h1>
+    <h1>Name:</h1>
+    <h3>{{name}}</h3>
+    <h1>Ingredients:</h1>
+    <ul>
+      <li *ngFor="let currentIngredients of ingredients">{{currentIngredients.ingredients}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
-
+  name: string = 'Torta';
+  ingredients: Ingredient[] = [
+    new Ingredient('8 large garlic cloves, minced'),
+    new Ingredient('2/3 cup orange juice'),
+    new Ingredient('2/3 cup lime juice'),
+    new Ingredient('2 tablespoons achiote seasoning paste'),
+    new Ingredient('2 tablespoons olive oil'),
+    new Ingredient('2 tsp. kosher salt, divided'),
+    new Ingredient('2 teaspoons Mexican oregano'),
+    new Ingredient('1 whole chicken')
+  ];
 }
 
-export class Task {
+export class Ingredient {
   public done: boolean = false;
-  constructor(public description: string) { }
+  constructor(public ingredients: string) { }
 }
